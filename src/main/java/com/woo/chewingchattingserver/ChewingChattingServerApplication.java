1e10531a457f599e32c555fbfb713ac06ca7b726
woo <chlguddn7@gmail.com>
2023-11-29 22:14:25 +0900

    add: init

---
+++ src/main/java/com/woo/chewingchattingserver/ChewingChattingServerApplication.java
@@ -0,0 +1,13 @@
+package com.woo.chewingchattingserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChewingChattingServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChewingChattingServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/woo/chewingchattingserver/ChewingChattingServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.woo.chewingchattingserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChewingChattingServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
